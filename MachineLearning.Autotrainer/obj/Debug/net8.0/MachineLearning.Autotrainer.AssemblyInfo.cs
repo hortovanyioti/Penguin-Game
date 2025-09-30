05398526bb60477db1c02aa7839f11c3c2a6837a
hortovanyioti <105929199+hortovanyioti@users.noreply.github.com>
2025-09-30 19:57:12 +0200

    Update material versions and add terrain material

--- MachineLearning.Autotrainer/obj/Debug/net8.0/MachineLearning.Autotrainer.AssemblyInfo.cs
+++ MachineLearning.Autotrainer/obj/Debug/net8.0/MachineLearning.Autotrainer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MachineLearning.Autotrainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabb7500eb97f9db39b96ee839dabc08c5fa63dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2acfa7e11e3facbab7bdc790fcf191bea728d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MachineLearning.Autotrainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MachineLearning.Autotrainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
