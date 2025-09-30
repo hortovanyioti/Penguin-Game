f4b43c481d5a1972ab426a6682dc3f61f18c9188
hortovanyioti <105929199+hortovanyioti@users.noreply.github.com>
2025-09-30 20:21:23 +0200

    Update assembly info and cache for new build
    AssemblyInformationalVersion and AssemblyInfoInputs.cache updated to reflect the latest build. No source code changes included.

--- MachineLearning.Autotrainer/obj/Debug/net8.0/MachineLearning.Autotrainer.AssemblyInfo.cs
+++ MachineLearning.Autotrainer/obj/Debug/net8.0/MachineLearning.Autotrainer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MachineLearning.Autotrainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd75fa2c0a5711abaeccfc6342a90df5622796e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2dbaa14968b841b6b22bd2d5da06c29ad4cbb60")]
 [assembly: System.Reflection.AssemblyProductAttribute("MachineLearning.Autotrainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MachineLearning.Autotrainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
